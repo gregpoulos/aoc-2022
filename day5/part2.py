f4ded059a8577c21406348faa6c868e3401bf25e
Greg Poulos <>
2022-12-05 23:53:02 -0800

    day 5

--- day5/part2.py
+++ day5/part2.py
@@ -1,8 +1,27 @@
 #!/usr/local/bin/python3
 
-counter = 0
-for line in open('input.txt', 'r').readlines():
-  l = line.strip()
-  counter += 1
+import re
 
-print(counter)
+stacks = [
+  [],
+  ['Q', 'F', 'M', 'R', 'L', 'W', 'C', 'V'],
+  ['D', 'Q', 'L'],
+  ['P', 'S', 'R', 'G', 'W', 'C', 'N', 'B'],
+  ['L', 'C', 'D', 'H', 'B', 'Q', 'G'],
+  ['V', 'G', 'L', 'F', 'Z', 'S'],
+  ['D', 'G', 'N', 'P'],
+  ['D', 'Z', 'P', 'V', 'F', 'C', 'W'],
+  ['C', 'P', 'D', 'M', 'S'],
+  ['Z', 'N', 'W', 'T', 'V', 'M', 'P', 'C'],
+]
+
+for line in open('input.txt', 'r').readlines():
+  n, source, dest = [int(x) for x in re.findall(r'\d+', line)]
+  lifted = []
+  for _ in range(n):
+    val = stacks[source].pop()
+    lifted.insert(0, val)
+  for x in lifted:
+    stacks[dest].append(x)
+
+print(''.join([stack.pop() for stack in stacks[1:10]]))
